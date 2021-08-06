519377ed17c49f1d4e74b5014c900b6728734b49
Muhammad Fathur Rahman <fathur@xoxzo.com>
2021-08-06 11:20:43 +0800

    Step 3. Preparing required parameters for GETOTP API

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,7 @@ Route::middleware('auth')->group(function () {
 
 // GETOTP callback route
 
+Route::get('/credit_transfers/{transfer_ref}/success', [CreditTransferController::class, 'otpSuccess'])->name('credit_transfers.success');
+Route::get('/credit_transfers/{transfer_ref}/failed', [CreditTransferController::class, 'otpFailed'])->name('credit_transfers.failed');
+
 require __DIR__.'/auth.php';
